a738a48f78b161e344eb45f0b896f611afacc657
Abi Concepcion <concepcionabigail03@gmail.com>
2025-12-06 08:27:07 +0800

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,10 +1,10 @@
+import { createClient } from '@supabase/supabase-js'
 
-import { createClient } from '@supabase/supabase-js';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
+if (!supabaseUrl || !supabaseKey) {
+  console.error("Missing Supabase Environment Variables! Check your .env or Vercel settings.")
+}
 
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase
-        
+export const supabase = createClient(supabaseUrl, supabaseKey)
